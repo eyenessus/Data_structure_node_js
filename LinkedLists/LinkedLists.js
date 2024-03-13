641b8ac67e95943f06e887b140ffa3a5f066fc13
Emerson S <eyenessus@gmail.com>
2024-03-13 20:50:00 -0300

    Lista ligada adicionando cabeças da lista

--- LinkedLists/LinkedLists.js
+++ LinkedLists/LinkedLists.js
@@ -1,20 +1,17 @@
 const Node = require('./Node');
 
 class LinkedList {
-  
   constructor() {
-    this.head = null;
+    this.head = null
   }
-
   addToHead(data) {
-    const newHead = new Node(data);
-    const currentHead = this.head;
-    this.head = newHead;
+    const newHead = new Node(data)
+    const currentHead = this.head
+    this.head = newHead
     if (currentHead) {
-      this.head.setNextNode(currentHead);
+      newHead.setNextNode(currentHead)
     }
   }
-
 }
 
 module.exports = LinkedList;
