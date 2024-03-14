25ea1b1a89c11a6d71db5976200dab0ee292d89a
Emerson S <eyenessus@gmail.com>
2024-03-13 23:05:02 -0300

    Adicionando propriedades da doubly list

--- src/LinkedLists/DoublyLinkedList.ts
+++ src/LinkedLists/DoublyLinkedList.ts
@@ -1,3 +1,22 @@
-class DoublyLinkedList{
+import { NodeM } from "../Node/NodeM";
+
+export class DoublyLinkedList {
+    head:NodeM|null;
+    tail:NodeM|null;
     
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    printList() {
+        let currentNode = this.head;
+        let output = '<head> ';
+        while (currentNode !== null) {
+            output += currentNode.data + ' ';
+            currentNode = currentNode.getNextNode();
+        }
+        output += '<tail>';
+        console.log(output);
+    }
 }
