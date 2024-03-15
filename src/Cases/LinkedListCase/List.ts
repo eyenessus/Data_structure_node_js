09a6c1f9ceeb641fbd7da8e2526838c619bd189b
Emerson S <eyenessus@gmail.com>
2024-03-14 23:11:57 -0300

    Casos de uso com lista ligadas adicionado

---
+++ src/Cases/LinkedListCase/List.ts
@@ -0,0 +1,13 @@
+const seasons = new LinkedList();
+seasons.printList();
+
+seasons.addToHead('summer')
+seasons.addToHead('spring')
+seasons.printList();
+
+seasons.addToTail('fall')
+seasons.addToTail('winter')
+seasons.printList();
+
+seasons.removeHead()
+seasons.printList();
--- src/Cases/NodeCase/Node.ts
+++ src/Cases/NodeCase/Node.ts
@@ -1,7 +1,3 @@
-
-/**
- * NODE
- */
 const strawberryNode = new NodeM('Berry Tasty')
 const vanillaNode = new NodeM('Vanilla')
 const coconutNode = new NodeM('Coconuts for Coconut')
@@ -14,29 +10,3 @@ let currentNode: NodeM | null = vanillaNode
 while (currentNode !== null) {
     currentNode = currentNode.getNextNode()
 }
-
-
-/**
- * Listas ligada
- */
-const seasons = new LinkedList();
-seasons.printList();
-
-seasons.addToHead('summer')
-seasons.addToHead('spring')
-seasons.printList();
-
-seasons.addToTail('fall')
-seasons.addToTail('winter')
-seasons.printList();
-
-seasons.removeHead()
-seasons.printList();
-
-
-/**
- * Listas duplamente ligadas
- */
-
-
-
