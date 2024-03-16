1736af1c7c709b46b9cb96ec6351299208444454
Emerson S <eyenessus@gmail.com>
2024-03-16 17:56:17 -0300

    Recursive

---
+++ src/Structure/Recursive/Recursive.ts
@@ -0,0 +1,25 @@
+const recursiveFactorial = (n) => {
+    if (n === 0) {
+      return 1;
+    } else if (n > 0) {
+      return n * recursiveFactorial(n - 1);
+    }
+  }
+  
+  const iterativeFactorial = (n) => {
+    result = 1;
+    while(n > 0) {
+      result *= n;
+      n -= 1;
+    }
+    return result;
+  }
+  
+  
+  
+  
+  
+  module.exports = {
+    recursiveFactorial,
+    iterativeFactorial
+  };
