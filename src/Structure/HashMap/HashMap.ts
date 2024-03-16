421b2d9e1f9bb965e4d10838e95020494a089fa6
Emerson S <eyenessus@gmail.com>
2024-03-16 10:46:20 -0300

    Função hash adicionada

--- src/Structure/HashMap/HashMap.ts
+++ src/Structure/HashMap/HashMap.ts
@@ -1,8 +1,16 @@
 class HashMap implements IHashMap{
     private hashmap: Array<Number>;
+
     constructor(size=0){
         this.hashmap = new Array(size)
         .fill(null)
     }
-    
+    hash(key:string) {
+        let hashCode = 0;
+        for (let i = 0; i < key.length; i++) {
+          hashCode += hashCode + key.charCodeAt(i);
+        }
+        return hashCode;
+      }
+
 }
