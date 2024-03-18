8a80922afac77a9e6fe1c6df38cbe2ba284d453c
Emerson S <eyenessus@gmail.com>
2024-03-18 20:59:13 -0300

    Remover filho

--- src/Nonlinear/Trees/TreeNode.ts
+++ src/Nonlinear/Trees/TreeNode.ts
@@ -1,18 +1,33 @@
 import NodeM from "../../Structure/Node/NodeM";
 
 class TreeNode {
-    data:any;
-    children:TreeNode[];
+  data: any;
+  children: TreeNode[];
 
-    constructor(data:any){
-        this.data = data;
-        this.children =[]
+  constructor(data: any) {
+    this.data = data;
+    this.children = []
+  }
+  addChild(child: NodeM | any) {
+    if (child instanceof TreeNode) {
+      this.children.push(child);
+    } else {
+      this.children.push(new TreeNode(child));
+    }
+  }
+  removeChild(childToRemove: any | NodeM) {
+    const length = this.children.length;
+    
+    this.children = this.children.filter(child => {
+      if (childToRemove instanceof TreeNode) {
+        return childToRemove !== child;
+      } else {
+        return child.data !== childToRemove;
       }
-      addChild(child:NodeM|any) {
-        if (child instanceof TreeNode) {
-          this.children.push(child);
-        } else {
-          this.children.push(new TreeNode(child));
-        }
-      }
+    });
+
+    if (length === this.children.length) {
+      this.children.forEach(child => child.removeChild(childToRemove));
+    }
+  }
 };
