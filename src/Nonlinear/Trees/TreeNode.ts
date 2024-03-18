094e4664bd9c4ea9b19fc1edfc730163ecc99b8e
Emerson S <eyenessus@gmail.com>
2024-03-17 22:42:30 -0300

    metodo para adicionar filho na arvore

--- src/Nonlinear/Trees/TreeNode.ts
+++ src/Nonlinear/Trees/TreeNode.ts
@@ -1,6 +1,18 @@
+import NodeM from "../../Structure/Node/NodeM";
+
 class TreeNode {
-    constructor(data){
+    data:any;
+    children:TreeNode[];
+
+    constructor(data:any){
         this.data = data;
         this.children =[]
+      }
+      addChild(child:NodeM|any) {
+        if (child instanceof TreeNode) {
+          this.children.push(child);
+        } else {
+          this.children.push(new TreeNode(child));
+        }
       }
 };
