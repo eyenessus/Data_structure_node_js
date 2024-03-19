3ffce90d4e376b270791405bdd2d6c1f259c99c7
Emerson S <eyenessus@gmail.com>
2024-03-18 21:45:27 -0300

    Travessia de largura

--- src/Nonlinear/Trees/TreeNode.ts
+++ src/Nonlinear/Trees/TreeNode.ts
@@ -30,8 +30,33 @@ class TreeNode {
       this.children.forEach(child => child.removeChild(childToRemove));
     }
   }
-  depthFirstTraversal() {
+  depthFirstTraversal() {//travessia em profundidade
     console.log(this.data);
     this.children.forEach(child => child.depthFirstTraversal());
   }
-};
+  breadthFirstTraversal() {
+    let queue = [this];
+    while (queue.length > 0) {
+      const current: NodeM | any = queue.shift();
+      console.log(current.data);
+      queue = queue.concat(current.children);
+    }
+  }
+  /*
+  O código selecionado é um método chamado broadthFirstTraversal dentro da classe TreeNode. Este método realiza uma travessia de pesquisa em largura de uma estrutura em árvore.
+
+Veja como funciona:
+
+Inicializa uma fila com o nó raiz da árvore.
+Ele entra em um loop while que continua enquanto houver nós na fila.
+Dentro do loop, ele remove o primeiro nó da fila usando o método shift e o atribui à variável atual.
+Em seguida, imprime a propriedade data do nó atual.
+Por fim, adiciona todos os filhos do nó atual ao final da fila usando o método concat.
+Ao realizar esse percurso, o método visita cada nó da árvore nível por nível, começando pela raiz e descendo até as folhas.
+
+Este método é útil para tarefas que exigem visitar todos os nós de uma árvore de maneira ampla, como pesquisar um valor específico ou imprimir o conteúdo da árvore de forma estruturada.
+
+Chamando esse método em um objeto TreeNode, você pode observar o percurso em largura da árvore. *
+*/
+
+}
