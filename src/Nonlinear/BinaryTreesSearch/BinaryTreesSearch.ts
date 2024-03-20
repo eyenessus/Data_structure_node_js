76ed9e370a012db4df8b1eb0d97f3a57bf605329
Emerson S <eyenessus@gmail.com>
2024-03-19 22:27:22 -0300

    Atravessando arvore

--- src/Nonlinear/BinaryTreesSearch/BinaryTreesSearch.ts
+++ src/Nonlinear/BinaryTreesSearch/BinaryTreesSearch.ts
@@ -38,4 +38,13 @@ export class BinaryTreeSearch {
         return null;
 
     }
+    depthFirstTraversal() {
+        if (this.left) {
+          this.left.depthFirstTraversal();
+        }
+        console.log(`Depth=${this.depth}, Value=${this.value}`);
+        if (this.right) {
+          this.right.depthFirstTraversal();
+        }
+      }
 };
