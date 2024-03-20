33de53327666c11f6425277c5c35e503259d493e
Emerson S <eyenessus@gmail.com>
2024-03-19 22:14:02 -0300

    Recuperando valor

--- src/Cases/BinaryTree.ts
+++ src/Cases/BinaryTree.ts
@@ -26,6 +26,24 @@ const target = 41;
 
 //   console.log(`The target index is ${targetIndex}.`);
 
+
+const BinaryTree = require('./BinaryTree');
+
+// create a BinaryTree
+const bt = new BinaryTree(100);
+
+// insert values to the BinaryTree
+bt.insert(50);
+bt.insert(125);
+bt.insert(75);
+bt.insert(25);
+
+// search for value 75 in BinaryTree
+let node = bt.getNodeByValue(75);
+console.log(node);
+
+// search for a non-existent value in BinaryTree
+node = bt.getNodeByValue(55);
+console.log(node);
+
 module.exports = binarySearch;
-
-
--- src/Nonlinear/BinaryTreesSearch/BinaryTreesSearch.ts
+++ src/Nonlinear/BinaryTreesSearch/BinaryTreesSearch.ts
@@ -1,29 +1,41 @@
+import NodeM from "../../Structure/Node/NodeM";
+
 export class BinaryTreeSearch {
     value: number;
     depth: number;
     left: BinaryTreeSearch | null;
     right: BinaryTreeSearch | null;
 
-    constructor(value:number,depth:number=1){
-      this.value= value
-      this.depth = depth
-      this.left =null
-      this.right=null
+    constructor(value: number, depth: number = 1) {
+        this.value = value
+        this.depth = depth
+        this.left = null
+        this.right = null
     }
-    insert(value) {
+    insert(value: number) {
         if (value < this.value) {
-          if (!this.left) {
-            this.left = new BinaryTree(value, this.depth + 1);
-          } else {
-            this.left.insert(value);
-          }
+            if (!this.left) {
+                this.left = new BinaryTreeSearch(value, this.depth + 1);
+            } else {
+                this.left.insert(value);
+            }
         } else {
-          if (!this.right) {
-            this.right = new BinaryTree(value, this.depth + 1);
-          } else {
-            this.right.insert(value);
-          }
+            if (!this.right) {
+                this.right = new BinaryTreeSearch(value, this.depth + 1);
+            } else {
+                this.right.insert(value);
+            }
         }
-      }
-  };
-  
+    }
+    getNodeByValue(value:number) :any{
+        if (this.value === value) {
+            return this;
+        } else if ((this.left) && (value < this.value)) {
+            return this.left.getNodeByValue(value);
+        } else if (this.right) {
+            return this.right.getNodeByValue(value);
+        }
+        return null;
+
+    }
+};
