99dc02ff065e6f7fa6fe24ab42da4981171874b3
E.S <eyenessus@gmail.com>
2024-03-30 23:05:03 -0300

    [FEATURE] Adicionando arestas conectando com vertices

--- src/Graph/Graphs/Vertex.ts
+++ src/Graph/Graphs/Vertex.ts
@@ -5,7 +5,13 @@ export class Vertex {
         this.data = data;
         this.edges = [];
       }
-    
+      addEdge(vertex) {
+        if (vertex instanceof Vertex) {
+          this.edges.push(new Edge(this, vertex));
+        } else {
+          throw new Error("Edge start and end must both be Vertex");
+        }
+      }
       print() {
         const edgeList = this.edges.map(edge =>
             edge.weight !== null ? `${edge.end.data} (${edge.weight})` : edge.end.data);
